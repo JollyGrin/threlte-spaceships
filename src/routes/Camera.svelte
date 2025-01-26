3755ba5dd7ddfd1c2f2cd6121f146b3880325cb2
jollygrin <JollyGrin@protonmail.com>
2025-01-26 19:42:25 +0100

    hello world

---
+++ src/routes/Camera.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { T } from '@threlte/core';
+	import { OrbitControls } from '@threlte/extras';
+</script>
+
+<T.PerspectiveCamera makeDefault position={[0, 10, 15]} fov={45} zoom={1.75}>
+	<OrbitControls autoRotate={false} enableZoom={true} enableDamping autoRotateSpeed={0.1} />
+</T.PerspectiveCamera>
---
+++ src/routes/Scene.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Cube from '$lib/components/Cube.svelte';
+	import Camera from './Camera.svelte';
+	import Lights from './Lights.svelte';
+</script>
+
+<svelte:window
+	on:keydown|preventDefault={(e) => {
+		console.log(e.code);
+		// DEBUGGER - adjust values here to test interactions
+		// e.code === "KeyA" && incrementPlayerScore("p1");
+	}}
+/>
+
+<Lights />
+<Camera />
+
+<Cube position={[0, 0, 0]} />
