155e1d21d6cec3ad18be6f9e1a5ba85d6d0cbcf7
jollygrin <JollyGrin@protonmail.com>
2025-01-26 23:08:20 +0100

    add types

--- src/routes/Scene.svelte
+++ src/routes/Scene.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { T } from '@threlte/core';
+	import type { Vector3Tuple } from 'three';
 	import Camera from './Camera.svelte';
 	import Lights from './Lights.svelte';
 	import { EdgesGeometry, BoxGeometry } from 'three';
@@ -7,13 +8,17 @@
 	let boxGeometry = new BoxGeometry(1, 1, 1);
 	let edgesGeometry = new EdgesGeometry(boxGeometry);
 
+	type CubeProps = {
+		position: Vector3Tuple;
+	};
+
 	$effect(() => {
 		edgesGeometry = new EdgesGeometry(boxGeometry);
 	});
 </script>
 
 <svelte:window
-	on:keydown|preventDefault={(e) => {
+	on:keydown|preventDefault={(e: KeyboardEvent) => {
 		console.log(e.code);
 		// DEBUGGER - adjust values here to test interactions
 		// e.code === "KeyA" && incrementPlayerScore("p1");
@@ -23,7 +28,7 @@
 <Lights />
 <Camera />
 
-{#snippet cube(props)}
+{#snippet cube(props: CubeProps)}
 	<T.LineSegments {...props}>
 		<T.BufferGeometry attributes={{position: edgesGeometry.attributes.position}} />
 		<T.LineBasicMaterial color="#ffffff" linewidth={2} />
